ec8e43bf82378f96d686a0a478874fceb4be9625
NicoZeitz <63420648+NicoZeitz@users.noreply.github.com>
2024-04-25 18:10:08 +0200

    add page

--- Rezeptverwaltung/Server/Components/ShoppingListDetailPage.cs
+++ Rezeptverwaltung/Server/Components/ShoppingListDetailPage.cs
@@ -1,13 +1,32 @@
 ﻿
 
+using Core.Entities;
+using Core.ValueObjects;
 using Server.Resources;
 
 namespace Server.Components;
 
 public class ShoppingListDetailPage(TemplateLoader templateLoader) : ContainerComponent(templateLoader)
 {
-    public override Task<string> RenderAsync()
+    public const string HEADER_SLOT = "Header";
+
+    public IEnumerable<(Portion, Recipe)> Recipes { get; set; } = [];
+    public IEnumerable<WeightedIngredient> Ingredients { get; set; } = [];
+    public ShoppingList? ShoppingList { get; set; }
+    public Chef? CurrentChef { get; set; }
+
+    public override async Task<string> RenderAsync()
     {
-        throw new NotImplementedException();
+        return await templateLoader
+           .LoadTemplate("ShoppingListDetailPage.html")
+           .RenderAsync(new
+           {
+               ShoppingList,
+               CurrentChef,
+               Recipes,
+               Ingredients,
+               Header = await GetRenderedSlottedChild(HEADER_SLOT),
+           })
+           .AsTask();
     }
 }
