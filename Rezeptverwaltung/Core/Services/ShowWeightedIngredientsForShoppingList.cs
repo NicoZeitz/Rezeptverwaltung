bde4e5729bebb36bdf3f63af2ba67875042a8e93
NicoZeitz <63420648+NicoZeitz@users.noreply.github.com>
2024-04-26 08:36:29 +0200

    Refactoring: Extract Method in Ingredient Calculation for Shopping List
    This refactoring replaces one long method for calculating the ingredients
    that are weighted by the number of portions requested for a shopping list
    with multiple smaller methods. This makes the code easier to read and
    understand.
    
    Co-authored-by: Fabian Wolf <fabianwlf@outlook.de>

--- Rezeptverwaltung/Core/Services/ShowWeightedIngredientsForShoppingList.cs
+++ Rezeptverwaltung/Core/Services/ShowWeightedIngredientsForShoppingList.cs
@@ -22,79 +22,111 @@ public class ShowWeightedIngredientsForShoppingList
     public IEnumerable<WeightedIngredient> ShowIngredients(ShoppingList shoppingList, Chef? viewer)
     {
         var recipesWithPortion = showPortionedRecipesFromShoppingList.ShowRecipes(shoppingList, viewer);
+        var uniqueRecipesWithPortion = DeduplicateRecipes(recipesWithPortion);
+        var weightedIngredientWithPortion = ScaleIngredientsByRecipePortion(uniqueRecipesWithPortion);
+        var weightedIngredients = GroupWeightedIngredientsWithPortionByIngredient(weightedIngredientWithPortion);
+        var orderedWeightedIngredients = OrderWeightedIngredients(weightedIngredients);
+        var combinedMeasurementUnits = CombineMeasurementUnits(orderedWeightedIngredients);
+        return combinedMeasurementUnits;
+    }
 
+    private IDictionary<Recipe, Portion> DeduplicateRecipes(IEnumerable<(Portion, Recipe)> recipesWithPortion)
+    {
         var uniqueRecipesWithPortion = new Dictionary<Recipe, Portion>();
 
         foreach (var (portion, recipe) in recipesWithPortion)
         {
-            if (uniqueRecipesWithPortion.TryGetValue(recipe, out var existingPortion))
-            {
-                uniqueRecipesWithPortion[recipe] = new Portion(existingPortion.Amount + portion.Amount);
-            }
-            else
-            {
-                uniqueRecipesWithPortion[recipe] = portion;
-            }
+            var existingPortion = uniqueRecipesWithPortion.GetValueOrDefault(recipe, new Portion(Rational<int>.Zero));
+            uniqueRecipesWithPortion[recipe] = new Portion(existingPortion.Amount + portion.Amount);
         }
 
-        var weightedIngredientWithPortion = new List<(Ingredient, MeasurementUnit, Rational<int>)>();
+        return uniqueRecipesWithPortion;
+    }
 
-        foreach (var (recipe, portion) in uniqueRecipesWithPortion)
+    private IEnumerable<(Ingredient, MeasurementUnit, Rational<int>)> ScaleIngredientsByRecipePortion(IDictionary<Recipe, Portion> portionedRecipes)
+    {
+        foreach (var (recipe, portion) in portionedRecipes)
         {
             var recipePersonAmount = portion.Amount / recipe.Portion.Amount;
 
             foreach (var ingredient in recipe.WeightedIngredients)
             {
-                weightedIngredientWithPortion.Add((ingredient.Ingredient, ingredient.PreparationQuantity, recipePersonAmount));
-            }
-        }
-
-        var weightedIngredients = new Dictionary<Ingredient, List<PortionedMeasurementUnits>>();
-
-        foreach (var (ingredient, preparationQuantity, recipePersonAmount) in weightedIngredientWithPortion)
-        {
-            var portionedMeasurementUnit = new PortionedMeasurementUnits(recipePersonAmount, preparationQuantity);
-
-            if (weightedIngredients.TryGetValue(ingredient, out var existingQuantities))
-            {
-                existingQuantities.Add(portionedMeasurementUnit);
-            }
-            else
-            {
-                weightedIngredients[ingredient] = [portionedMeasurementUnit];
-            }
-        }
-
-        var neutralCombinedMeasurementUnit = new CombinedMeasurementUnit(Rational<int>.Zero, Rational<int>.Zero);
-
-        foreach (var (ingredient, portionedMeasurementUnits) in weightedIngredients.OrderBy(weightedIngredient => weightedIngredient.Key))
-        {
-            var combinedMeasurementUnit = neutralCombinedMeasurementUnit;
-
-            foreach (var portionedMeasurementUnit in portionedMeasurementUnits)
-            {
-                combinedMeasurementUnit = portionedMeasurementUnit.MeasurementUnit.Combine(combinedMeasurementUnit, portionedMeasurementUnit.Portion);
-            }
-
-            if (combinedMeasurementUnit.Count != Rational<int>.Zero)
-            {
-                var pieces = ceilRationalService.CeilRational(combinedMeasurementUnit.Count);
-                yield return new WeightedIngredient(
-                    new Piece((uint)pieces),
-                    ingredient
-                );
-            }
-
-            if (combinedMeasurementUnit.Weight != Rational<int>.Zero)
-            {
-                var weight = ceilRationalService.CeilRational(combinedMeasurementUnit.Weight);
-                yield return new WeightedIngredient(
-                    new Weight((uint)weight),
-                    ingredient
-                );
+                yield return (ingredient.Ingredient, ingredient.PreparationQuantity, recipePersonAmount);
             }
         }
     }
 
-    private record struct PortionedMeasurementUnits(Rational<int> Portion, MeasurementUnit MeasurementUnit);
+    private IDictionary<Ingredient, List<PortionedMeasurementUnit>> GroupWeightedIngredientsWithPortionByIngredient(IEnumerable<(Ingredient, MeasurementUnit, Rational<int>)> weightedIngredientsWithPortion)
+    {
+        var weightedIngredients = new Dictionary<Ingredient, List<PortionedMeasurementUnit>>();
+
+        foreach (var (ingredient, preparationQuantity, recipePersonAmount) in weightedIngredientsWithPortion)
+        {
+            var portionedMeasurementUnit = new PortionedMeasurementUnit(recipePersonAmount, preparationQuantity);
+            var existingPortionedMeasurementUnits = weightedIngredients.GetValueOrDefault(ingredient, []);
+            existingPortionedMeasurementUnits.Add(portionedMeasurementUnit);
+            weightedIngredients[ingredient] = existingPortionedMeasurementUnits;
+        }
+
+        return weightedIngredients;
+    }
+
+    private IDictionary<Ingredient, List<PortionedMeasurementUnit>> OrderWeightedIngredients(IDictionary<Ingredient, List<PortionedMeasurementUnit>> weightedIngredients)
+    {
+        return weightedIngredients
+            .OrderBy(weightedIngredient => weightedIngredient.Key)
+            .ToDictionary();
+    }
+
+    private IEnumerable<WeightedIngredient> CombineMeasurementUnits(IDictionary<Ingredient, List<PortionedMeasurementUnit>> weightedIngredients)
+    {
+        foreach (var (ingredient, portionedMeasurementUnits) in weightedIngredients)
+        {
+            var combinedMeasurementUnit = CombineSingleMeasurementUnit(portionedMeasurementUnits);
+
+            if (combinedMeasurementUnit.Count != Rational<int>.Zero)
+            {
+                yield return CreateNewWeightedPieceFrom(ingredient, combinedMeasurementUnit);
+            }
+
+            if (combinedMeasurementUnit.Weight != Rational<int>.Zero)
+            {
+                yield return CreateNewWeightedWeightFrom(ingredient, combinedMeasurementUnit);
+            }
+        }
+    }
+
+    private CombinedMeasurementUnit CombineSingleMeasurementUnit(IEnumerable<PortionedMeasurementUnit> portionedMeasurementUnits)
+    {
+        var neutralCombinedMeasurementUnit = new CombinedMeasurementUnit(Rational<int>.Zero, Rational<int>.Zero);
+
+        var combinedMeasurementUnit = neutralCombinedMeasurementUnit;
+
+        foreach (var portionedMeasurementUnit in portionedMeasurementUnits)
+        {
+            combinedMeasurementUnit = portionedMeasurementUnit.MeasurementUnit.Combine(combinedMeasurementUnit, portionedMeasurementUnit.Portion);
+        }
+
+        return combinedMeasurementUnit;
+    }
+
+    private WeightedIngredient CreateNewWeightedPieceFrom(Ingredient ingredient, CombinedMeasurementUnit combinedMeasurementUnit)
+    {
+        var piece = ceilRationalService.CeilRational(combinedMeasurementUnit.Count);
+        return new WeightedIngredient(
+            new Piece((uint)piece),
+            ingredient
+        );
+    }
+
+    private WeightedIngredient CreateNewWeightedWeightFrom(Ingredient ingredient, CombinedMeasurementUnit combinedMeasurementUnit)
+    {
+        var weight = ceilRationalService.CeilRational(combinedMeasurementUnit.Weight);
+        return new WeightedIngredient(
+            new Weight((uint)weight),
+            ingredient
+        );
+    }
+
+    private record struct PortionedMeasurementUnit(Rational<int> Portion, MeasurementUnit MeasurementUnit);
 }
